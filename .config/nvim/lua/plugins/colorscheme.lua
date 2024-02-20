f41e276dcba695b23607c5292ba0b8418e6a6010
Ivan Yordanov <i.p.yordanov@proton.me>
2024-02-20 18:11:07 +0200

    Update Neovim coloscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,7 @@
 return  {
+  {
     "navarasu/onedark.nvim",
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
@@ -8,4 +10,18 @@ return  {
       }
       require('onedark').load()
     end,
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    name = "zenbones",
+    -- priority = 1000,
+    config = function()
+      vim.g.zenbones = { darkness = "stark", lightness = "bright" }
+      vim.cmd([[
+        set background=dark
+        colorscheme zenbones
+      ]])
+    end,
+    dependencies = { "rktjmp/lush.nvim" },
   }
+}
