c29bd9f31389f2885ee0a34b481536ee981f3eaa
Ivan Yordanov <hi@ivanyordanov.me>
2024-09-05 10:52:58 +0200

    Update Neovim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,6 +57,14 @@ cmp.setup({
   },
 })
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+
 -----------------------------------------------------------------------------//
 -- Utils
 -----------------------------------------------------------------------------//
