4192344aad2eb976dc80901b5b531221e296be37
Ivan Yordanov <ivan.yordanov@crystalloids>
2025-07-08 11:32:36 +0200

    Adjust zsh plugin directories

--- .config/zsh.d/plugins.zsh
+++ .config/zsh.d/plugins.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 # Syntax highlighting
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # Autosuggestions
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
