6d1b6ef63afdbb8f6209e368ab9140242251ee8e
Ivan Yordanov <ivan.yordanov@crystalloids>
2025-07-30 11:19:11 +0200

    Autocompletion for Google sdk

--- .config/zsh.d/autocompletion.zsh
+++ .config/zsh.d/autocompletion.zsh
@@ -1,2 +1,6 @@
 autoload -U +X bashcompinit && bashcompinit
 source $HOME/.config/zsh.d/autocompletion/az.completion
+
+# gcloud
+source "$(gcloud info --format='value(installation.sdk_root)')/path.zsh.inc"
+source "$(gcloud info --format='value(installation.sdk_root)')/completion.zsh.inc"
