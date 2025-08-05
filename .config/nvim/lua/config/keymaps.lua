f48d42d2ffb90a8122726b2dcb27638558b22246
Ivan Yordanov <ivan.yordanov@crystalloids>
2025-08-05 12:04:03 +0200

    Remove unused mapping

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,6 @@ vim.keymap.set('', 'Q', '') -- disable
 vim.keymap.set('n', 'x', '"_x') -- delete char without yank
 vim.keymap.set('x', 'x', '"_x') -- delete visual selection without yank
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- copy to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) -- delete without yank
 
 -- file explorer (set in the plugin)
 -- vim.keymap.set('n', '<leader>pv', vim.cmd.Lexplore)
