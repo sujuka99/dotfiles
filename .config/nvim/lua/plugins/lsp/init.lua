bf0566fe0eaace4214eaab654ff6bbfef7097f18
Ivan Yordanov <ivan.yordanov@crystalloids>
2025-08-07 13:53:55 +0200

    Add code completion

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,19 +1,82 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        opts = {}
+  {
+    'mason-org/mason.nvim',
+    opts = {},
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",   -- LSP completion
+      "hrsh7th/cmp-buffer",     -- Buffer words
+      "hrsh7th/cmp-path",       -- Filesystem paths
+      "hrsh7th/cmp-cmdline",    -- Cmdline completion
+      "L3MON4D3/LuaSnip",       -- Snippets
+      "saadparwaiz1/cmp_luasnip", -- Snippet integration
     },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-	        "lua_ls",
-		"basedpyright",
-	    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
-        dependencies = {
-            "mason-org/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' }
+            }
+          }
+        })
+      })
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    init = function()
+
+    end,
+    dependencies = {
+      "nvim-cmp",
+    }
+  },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        'lua_ls',
+        'basedpyright',
+      },
     },
+    dependencies = {
+      'mason-org/mason.nvim',
+      'neovim/nvim-lspconfig',
+    },
+  },
 }
